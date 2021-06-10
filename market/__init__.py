be5c6e4fc0fb31fb85ad8639749094c78d444370
Ashana <60913775+ashana1647@users.noreply.github.com>
2021-06-10 21:39:00 +0530

    Update __init__.py

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
